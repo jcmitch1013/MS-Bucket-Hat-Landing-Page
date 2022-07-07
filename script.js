97373688f7565843588f2d39211c7ccebbee741d
Joshua C Mitchell <joshua.mitchell2011@gmail.com>
2022-07-07 16:59:18 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+//When you click the small image
+//A bigger image is shown
+//add shadow to background
+
+//Module pop ups
+const leftMod = document.querySelector(".left_mod");
+const midMod = document.querySelector(".mid_mod");
+const rightMod = document.querySelector(".right_mod");
+
+const overlay = document.querySelector(".modal-images");
+
+//Image "buttons"
+const leftBtn = document.querySelector("#hat_left");
+const midBtn = document.querySelector("#mark_selfie");
+const rightBtn = document.querySelector("#hat_right");
+
+const closeModal = function () {
+  leftMod.classList.add("hidden");
+  midMod.classList.add("hidden");
+  rightMod.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+leftBtn.addEventListener("click", function () {
+  leftMod.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
+
+midBtn.addEventListener("click", function () {
+  midMod.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
+
+rightBtn.addEventListener("click", function () {
+  rightMod.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
+
+overlay.addEventListener("click", closeModal);
